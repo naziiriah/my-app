7b1495f5593e929fb96a79f79f3c614a351dd205
naziiriah <naziistry7@gmail.com>
2022-04-16 10:20:49 +0100

    Logic completed

--- src/games/gameLogics.tsx
+++ src/games/gameLogics.tsx
@@ -3,9 +3,8 @@ import React, { useEffect } from "react";
 import { useState } from "react"
 
 
-function randomGenerator(){
-   return Math.floor((Math.random() * 3) + 1);
-}
+const randomNumber = Math.floor((Math.random() * 3) + 1);
+
 
 export const FirstGamePlay = (props: { state: any }) => {
     const state = props.state
@@ -45,22 +44,25 @@ export const FirstGamePlay = (props: { state: any }) => {
 export const SelectedPiece = (   props: { count: any; state: any } ) => { 
     
     const count = props.count,
-    randomNumber = randomGenerator,
     state = props.state,
     circlehold = `circle-holder-${state[0]}`,
     img = `images-${state[0]}`,
     [info, setinfo] = useState('');
     const [display, setDisplay] = useState(false),
-    houseCircleHolder = `circle-holder-${randomNumber()}`,
-    houseImage = `images-${randomNumber()}`;
+    houseCircleHolder = `circle-holder-${randomNumber}`,
+    houseImage = `images-${randomNumber}`;
 
 useEffect(() =>{
-    if( (state[0] ===3 && randomNumber() === 1) || (state[0] === 1 && randomNumber() === 2) || (state[0] === 2 && randomNumber() === 3)  ){
-                    setinfo('you lose')
-                    count[1](count[0]--)
-    }else if ( (state[0] === 1 && randomNumber() === 3) || (state[0] === 2 && randomNumber() === 1) || (state[0] === 3 && randomNumber() === 2)  ) {
-                    setinfo('you win')
-                    count[1](count[0]--)
+    if( (state[0] ===3 && randomNumber === 1) 
+        || (state[0] === 1 && randomNumber === 2) 
+        || (state[0] === 2 && randomNumber === 3) ){
+            setinfo('you lose')
+            count[1](count[0]--)
+    }else if ( (state[0] === 1 && randomNumber === 3) 
+        || (state[0] === 2 && randomNumber === 1) 
+        || (state[0] === 3 && randomNumber === 2)  ) {
+                setinfo('you win')
+                count[1](count[0]++)
     }else if(state[0] === randomNumber){
         setinfo(' a draw')    
     }
@@ -72,9 +74,9 @@ useEffect(() =>{
 },[])  
 
 function refactor() {
-    state[1](0)
-    setinfo('')
-    setDisplay(false)
+    const PrevCount = localStorage.getItem('count')
+    localStorage.setItem('count', `${count[0] + Number(PrevCount)}`)
+    window.location.reload();
 }
 
     return(
@@ -82,7 +84,8 @@ function refactor() {
          <div className="selected">
             <div className='your-pick'>
                     <h1 className="picked"> you picked</h1>
-                    <div className='image-holder'>
+                    <div className='i
+                    mage-holder'>
                         <div className={circlehold}>
                             <div className={img} >
                             </div>
